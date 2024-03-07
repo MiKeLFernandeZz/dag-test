c1fc86c1f90df712e0a45a8e4d01979fbf646985
MiKeLFernandeZz <73524827+MiKeLFernandeZz@users.noreply.github.com>
2024-03-07 13:14:53 +0000

    test

--- dags/redwine_dag_k8s.py
+++ dags/redwine_dag_k8s.py
@@ -14,7 +14,7 @@ The DAG is scheduled to run every day at 12:00 AM.
 Please ensure that the necessary dependencies are installed and accessible for executing the tasks.
 
 test
-"""
+""" 
 
 from datetime import datetime
 from airflow.decorators import dag, task
